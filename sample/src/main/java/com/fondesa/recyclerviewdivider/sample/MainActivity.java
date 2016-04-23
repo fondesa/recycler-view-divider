9b9476c08e1edf7f2a0329e079c73129e1ccfa90
antoniolig <giorgio.antonioli.dev@gmail.com>
2016-04-23 11:13:41 +0200

    updated sample with list

--- sample/src/main/java/com/fondesa/recyclerviewdivider/sample/MainActivity.java
+++ sample/src/main/java/com/fondesa/recyclerviewdivider/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.fondesa.recyclerviewdivider.sample;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,7 +12,11 @@ import android.widget.TextView;
 
 import com.fondesa.recyclerviewdivider.RecyclerViewDivider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int LIST_SIZE = 20000;
 
     @SuppressWarnings("ConstantConditions")
     @Override
@@ -25,22 +30,30 @@ public class MainActivity extends AppCompatActivity {
         mFirstRecyclerView.addItemDecoration(new RecyclerViewDivider(Color.BLACK, 1));
         mSecondRecyclerView.addItemDecoration(new RecyclerViewDivider(Color.BLACK, 1));
 
-        final int[] dummyArray = new int[20];
-        for (int i = 0; i < dummyArray.length; i++) {
-            dummyArray[i] = i + 1;
+        final List<Integer> dummyList = new ArrayList<>(LIST_SIZE);
+        for (int i = 0; i < LIST_SIZE; i++) {
+            dummyList.add(i + 1);
         }
-        final DummyAdapter firstDummyAdapter = new DummyAdapter(dummyArray, true);
+        final DummyAdapter firstDummyAdapter = new DummyAdapter(true);
         mFirstRecyclerView.setAdapter(firstDummyAdapter);
-        final DummyAdapter secondDummyAdapter = new DummyAdapter(dummyArray, false);
+        final DummyAdapter secondDummyAdapter = new DummyAdapter(false);
         mSecondRecyclerView.setAdapter(secondDummyAdapter);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                firstDummyAdapter.updateList(dummyList);
+                secondDummyAdapter.updateList(dummyList);
+            }
+        }, 2000);
     }
 
     private class DummyAdapter extends RecyclerView.Adapter {
-        private int[] array;
+        private List<Integer> list;
         private boolean first;
 
-        public DummyAdapter(int[] array, boolean first) {
-            this.array = array;
+        public DummyAdapter(boolean first) {
+            this.list = new ArrayList<>();
             this.first = first;
         }
 
@@ -51,12 +64,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            ((DummyViewHolder) holder).setItemText(array[position]);
+            ((DummyViewHolder) holder).setItemText(list.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return array.length;
+            return list.size();
+        }
+
+        public void updateList(List<Integer> list) {
+            if (list != this.list) {
+                this.list = list;
+                notifyDataSetChanged();
+            }
         }
     }
 
