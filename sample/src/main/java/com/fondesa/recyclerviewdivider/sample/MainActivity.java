2c569d9f06b7fad30481549181c0cf9c2375c687
antoniolig <giorgio.antonioli.dev@gmail.com>
2016-05-01 01:03:55 +0200

    updated sample

--- sample/src/main/java/com/fondesa/recyclerviewdivider/sample/MainActivity.java
+++ sample/src/main/java/com/fondesa/recyclerviewdivider/sample/MainActivity.java
@@ -48,11 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
         secondDivider = RecyclerViewDivider.with(this)
                 .addTo(mSecondRecyclerView)
-                .color(Color.BLACK)
-//                .drawable(ContextCompat.getDrawable(this, R.drawable.ll_vertical_div))
+//                .color(Color.BLACK)
+                .drawable(ContextCompat.getDrawable(this, R.drawable.ll_vertical_div))
 //                .tint(Color.RED)
-                .size(getResources().getDimensionPixelSize(R.dimen.custom_size))
-                .asSpace()
+//                .size(getResources().getDimensionPixelSize(R.dimen.custom_size))
+//                .asSpace()
 //                .marginSize(getResources().getDimensionPixelSize(R.dimen.custom_size))
                 .build();
 
