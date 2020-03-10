f1b7a9b233fb2df767ee4f5486e8368b4e7d8568
antoniolig <giorgio.antonioli.dev@gmail.com>
2020-03-10 21:53:48 +0100

    Update copyright

--- recycler-view-divider/src/main/kotlin/com/fondesa/recyclerviewdivider/manager/tint/FixedTintManager.kt
+++ recycler-view-divider/src/main/kotlin/com/fondesa/recyclerviewdivider/manager/tint/FixedTintManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Fondesa
+ * Copyright (c) 2020 Fondesa
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
