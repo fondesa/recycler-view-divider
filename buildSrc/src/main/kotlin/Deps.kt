4e9af03059f3eecb563bc5681446cd05b98723dd
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-03-10 22:20:25 +0100

    New dependencies system (#49)

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 Fondesa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Deps {
+    const val androidPlugin = "com.android.tools.build:gradle:3.5.0"
+    const val androidxAppCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+    const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
+    const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50"
+    const val recyclerViewDivider = "com.github.fondesa:recycler-view-divider:3.1.0"
+}
