f2775d758cb0fb870b70583e095c70216c6f3fdd
antoniolig <giorgio.antonioli.dev@gmail.com>
2020-03-11 00:51:07 +0100

    Upgraded androidx.recyclerview:recyclerview from 1.0.0 to 1.1.0

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -17,7 +17,7 @@
 object Deps {
     const val androidPlugin = "com.android.tools.build:gradle:3.5.0"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.1.0"
-    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.0.0"
+    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
