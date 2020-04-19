c217f657ef8f1c1cff0decfad5a082c8ae3bf34a
antoniolig <giorgio.antonioli.dev@gmail.com>
2020-04-19 15:24:05 +0200

    Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.3.71 to 1.3.72

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -22,7 +22,7 @@ object Deps {
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.71"
-    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
     const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.4.2"
 }
