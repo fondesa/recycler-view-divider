d05b858732a0d703e5f61c0eddeed006bc87194f
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-09-09 01:10:57 +0200

    Upgraded org.jetbrains.kotlin:kotlin-stdlib-jdk7 from 1.3.72 to 1.4.0

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -15,15 +15,15 @@
  */
 
 object Deps {
-    const val androidPlugin = "com.android.tools.build:gradle:4.0.0"
-    const val androidxAppCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val androidPlugin = "com.android.tools.build:gradle:4.0.1"
+    const val androidxAppCompat = "androidx.appcompat:appcompat:1.2.0"
     const val androidxJUnit = "androidx.test.ext:junit:1.1.1"
     const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5"
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val junit = "junit:junit:4.13"
-    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72"
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.0"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
