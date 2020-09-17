8a137cb048bfc461a12d2111bca7812ad59d608a
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-09-17 21:15:23 +0200

    Upgraded org.jetbrains.kotlin:kotlin-stdlib-jdk7 from 1.4.0 to 1.4.10

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -23,7 +23,7 @@ object Deps {
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val junit = "junit:junit:4.13"
-    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.0"
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.10"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
