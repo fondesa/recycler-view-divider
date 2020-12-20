66550440404a356b2161a04344b7e6d92c843ac5
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-12-20 16:48:21 +0100

    Upgraded com.releaseshub:releases-hub-gradle-plugin from 1.6.0 to 1.6.1 (#139)

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -28,6 +28,6 @@ object Deps {
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.1"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
-    const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.6.0"
+    const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.6.1"
     const val robolectric = "org.robolectric:robolectric:4.4"
 }
