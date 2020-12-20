e09a16649a41e93b9bf903770a40f045195a41c0
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-12-20 17:09:41 +0100

    Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.3.72 to 1.4.21

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -24,7 +24,7 @@ object Deps {
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val junit = "junit:junit:4.13.1"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.21"
-    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.1"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
