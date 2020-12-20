c452a95879f94350ae99ae2610921db4109b5fc8
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-12-20 17:09:23 +0100

    Upgraded org.jetbrains.dokka:dokka-gradle-plugin from 0.9.18 to 1.4.20

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -20,7 +20,7 @@ object Deps {
     const val androidxJUnit = "androidx.test.ext:junit:1.1.2"
     const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5"
-    const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
+    const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.20"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val junit = "junit:junit:4.13.1"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72"
