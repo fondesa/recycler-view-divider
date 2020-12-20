927ea3af7f17cd101756b2a9574b15fed38fe333
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-12-20 17:05:38 +0100

    Upgraded org.jlleitschuh.gradle:ktlint-gradle from 9.2.1 to 9.4.1

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -15,19 +15,19 @@
  */
 
 object Deps {
-    const val androidPlugin = "com.android.tools.build:gradle:4.0.1"
+    const val androidPlugin = "com.android.tools.build:gradle:4.1.1"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.2.0"
     const val androidxJUnit = "androidx.test.ext:junit:1.1.2"
     const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5"
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
-    const val junit = "junit:junit:4.13.1"
+    const val junit = "junit:junit:4.13"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.1"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
-    const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.6.1"
+    const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.6.0"
     const val robolectric = "org.robolectric:robolectric:4.4"
 }
