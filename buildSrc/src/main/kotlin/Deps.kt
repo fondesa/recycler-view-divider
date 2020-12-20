12b23557f32ed52eebe85eac27fe5202f9512288
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-12-20 17:06:15 +0100

    Upgraded junit:junit from 4.13 to 4.13.1

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -15,7 +15,7 @@
  */
 
 object Deps {
-    const val androidPlugin = "com.android.tools.build:gradle:4.0.1"
+    const val androidPlugin = "com.android.tools.build:gradle:4.1.1"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.2.0"
     const val androidxJUnit = "androidx.test.ext:junit:1.1.2"
     const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0"
@@ -25,9 +25,9 @@ object Deps {
     const val junit = "junit:junit:4.13.1"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
-    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.1"
+    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
-    const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.6.1"
+    const val releasesHubPlugin = "com.releaseshub:releases-hub-gradle-plugin:1.6.0"
     const val robolectric = "org.robolectric:robolectric:4.4"
 }
