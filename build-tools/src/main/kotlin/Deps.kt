71e50f7496841807cb6dec59eacfeb53cc1c7aee
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-04-26 20:11:34 +0200

    Upgraded androidx.recyclerview:recyclerview from 1.1.0 to 1.2.0

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -19,7 +19,7 @@ object Deps {
     const val androidPluginApi = "com.android.tools.build:gradle-api:4.1.3"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.2.0"
     const val androidxJUnit = "androidx.test.ext:junit:1.1.2"
-    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0"
+    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.2.0"
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.30"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val gradleMavenPublishPlugin = "com.vanniktech:gradle-maven-publish-plugin:0.14.2"
