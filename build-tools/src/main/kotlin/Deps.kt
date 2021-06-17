3d4e45b7c991ffb0e5eef1b721619ad5b902b27c
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-06-17 23:04:02 +0200

    Upgraded dependencies for groupId org.jlleitschuh.gradle (#206)
    * Upgraded org.jlleitschuh.gradle:ktlint-gradle from 10.0.0 to 10.1.0
    
    * Remove experimental rules of ktlint

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -26,7 +26,7 @@ object Deps {
     const val junit = "junit:junit:4.13.2"
     const val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:1.4.32"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32"
-    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:10.0.0"
+    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:10.1.0"
     const val mockitoCore = "org.mockito:mockito-core:3.11.1"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
     const val recyclerViewDividerBuildTools = "com.fondesa.recyclerviewdivider.buildtools:build-tools"
