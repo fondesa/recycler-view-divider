67691bf3e622808e162ef6a039ccca18a54945c2
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-07-07 21:16:43 +0200

    Upgraded androidx.test.ext:junit from 1.1.2 to 1.1.3 (#213)

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -18,7 +18,7 @@ object Deps {
     const val androidPlugin = "com.android.tools.build:gradle:4.2.1"
     const val androidPluginApi = "com.android.tools.build:gradle-api:4.2.1"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.3.0"
-    const val androidxJUnit = "androidx.test.ext:junit:1.1.2"
+    const val androidxJUnit = "androidx.test.ext:junit:1.1.3"
     const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.2.1"
     const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
