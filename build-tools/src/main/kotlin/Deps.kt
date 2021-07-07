cd4bcfcd26f12c45f13579652f968f4aacd69895
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-07-07 21:18:04 +0200

    Upgraded dependencies for groupId com.android.tools.build (#212)
    * Upgraded com.android.tools.build:gradle from 4.2.1 to 4.2.2
    
    * Upgraded com.android.tools.build:gradle-api from 4.2.1 to 4.2.2

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -15,8 +15,8 @@
  */
 
 object Deps {
-    const val androidPlugin = "com.android.tools.build:gradle:4.2.1"
-    const val androidPluginApi = "com.android.tools.build:gradle-api:4.2.1"
+    const val androidPlugin = "com.android.tools.build:gradle:4.2.2"
+    const val androidPluginApi = "com.android.tools.build:gradle-api:4.2.2"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.3.0"
     const val androidxJUnit = "androidx.test.ext:junit:1.1.3"
     const val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.2.1"
